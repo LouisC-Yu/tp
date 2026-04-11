ed3418f44348b3a66ac8b010cce457fbe446a9db
oofyneril <lrwong369@gmail.com>
2026-04-12 03:11:33 +0800

    Fix coverage

--- src/main/java/seedu/address/logic/parser/ParserUtil.java
+++ src/main/java/seedu/address/logic/parser/ParserUtil.java
@@ -30,7 +30,7 @@ public class ParserUtil {
     public static Index parseIndex(String oneBasedIndex) throws ParseException {
         String trimmedIndex = oneBasedIndex.trim();
 
-        if (!StringUtil.isNonZeroUnsignedInteger(trimmedIndex)) {
+        if (!trimmedIndex.matches("[1-9]\\d*")) {
             throw new ParseException(MESSAGE_INVALID_INDEX);
         }
 
